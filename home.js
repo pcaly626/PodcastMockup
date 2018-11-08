5d371bc6dd69aa90ea06ef2b4ad8ac42f3f77f93
pcal626 <pcaly626@gmail.com>
2018-11-07 19:17:28 -0600

    Created a grid for the HTML

--- home.js
+++ home.js
@@ -1,2 +1,10 @@
 const soundcloud = require('soundcloud');
 
+function dropDownMenu(){
+	let click = document.getElementById("drop-down-menu");
+
+	if(click.className === "navbar-list")
+			click.className += " responsive";
+		else
+			click.className = "navbar-list";
+}
